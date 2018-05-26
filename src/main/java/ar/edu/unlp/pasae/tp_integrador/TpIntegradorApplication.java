64b9fa1363570b712c9af9fa451ec81765d6788a
Agustin Ortu <ortu.agustin@gmail.com>
2018-05-25 22:12:43 -0300

    importo proyecto spring boot con modulos h2, jpa y web

---
+++ src/main/java/ar/edu/unlp/pasae/tp_integrador/TpIntegradorApplication.java
@@ -0,0 +1,12 @@
+package ar.edu.unlp.pasae.tp_integrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntegradorApplication.class, args);
+	}
+}
---
+++ src/test/java/ar/edu/unlp/pasae/tp_integrador/TpIntegradorApplicationTests.java
@@ -0,0 +1,16 @@
+package ar.edu.unlp.pasae.tp_integrador;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TpIntegradorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
