8f27db67ba717c6f6e1f93a1361e962bdb347617
Agustin ORTU <ortu.agustin@gmail.com>
2018-07-10 11:53:04 -0300

    agrego TODOs

--- src/main/java/ar/edu/unlp/pasae/tp_integrador/config/SecurityConfiguration.java
+++ src/main/java/ar/edu/unlp/pasae/tp_integrador/config/SecurityConfiguration.java
@@ -1,5 +1,17 @@
 package ar.edu.unlp.pasae.tp_integrador.config;
 
+// TODO segurizar servicios: Patologia, Fenotipo, Usuarios
+// - Solamente puede acceder a este servicio usuarios con rol ADMIN
+// TODO segurizar servicio: Pacientes
+// - Solamente puede acceder a este servicio usuarios con rol REGISTER
+// - Update y Delete solamente puede hacerlo el usuario asociado al paciente (el que lo ingreso al sistema)
+// TODO segurizar servicio: Analisis de datos
+// - Solamente puede acceder a este servicio usuarios con rol SCIENTIST
+// TODO segurizar servicio: Gestion de asociaciones genotipo-fenotivo en estado publicado
+// - Solamente puede acceder a este servicio usuarios con rol CLINICAL_DOCTOR
+// TODO segurizar servicio: Visualizacion de graficos de fenotipo/genotipo
+// - Solamente puede acceder a este servicio usuarios con rol EPIDEMIOLOGIST
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -26,7 +38,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.cors().and()
 		.csrf().disable() // Necesario porque sino ignora los request que no son GET (ncesita el CRSF token)
 		.authorizeRequests()
-		//.antMatchers("/**").authenticated() 
+		//.antMatchers("/**").authenticated()
 		.antMatchers("/scientist/**").hasAuthority(RoleName.SCIENTIST.toString())
 //		.antMatchers("/admin/**").hasAuthority(RoleName.ADMIN.toString())
 		.antMatchers("/admin/**").permitAll()
@@ -34,7 +46,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.antMatchers("/epidemiologist/**").hasAuthority(RoleName.EPIDEMIOLOGIST.toString())
 		.antMatchers("/css/**", "/resources/**").permitAll()
 		//.antMatchers("/secured-admin/**").hasAnyAuthority(RoleName.ADMIN.toString(), RoleName.SCIENTIST.toString()) // Multiples roles
-		// Dejo las siguientes dos linea como ejemplo de la funcion "access()"        
+		// Dejo las siguientes dos linea como ejemplo de la funcion "access()"
 		//.antMatchers("/admin/**").access("hasRole('ADMIN')")
 		//.antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
 		.and().formLogin()
