b64ff872dc5f2d9da5d885d4340b3410b180e876
Agustin ORTU <ortu.agustin@gmail.com>
2018-07-10 11:05:15 -0300

    Implementada relacion patologia < --> fenotipos

--- src/main/java/ar/edu/unlp/pasae/tp_integrador/dtos/PathologyDTO.java
+++ src/main/java/ar/edu/unlp/pasae/tp_integrador/dtos/PathologyDTO.java
@@ -1,26 +1,28 @@
 package ar.edu.unlp.pasae.tp_integrador.dtos;
 
+import java.util.Set;
+import java.util.HashSet;
+
 import javax.validation.constraints.NotEmpty;
 
 public class PathologyDTO {
 	private Long id;
 	@NotEmpty
 	private String name;
+	private Set<NumericPhenotypeDTO> numericPhenotypes = new HashSet<NumericPhenotypeDTO>();
+	private Set<CategoricPhenotypeDTO> categoricPhenotypes = new HashSet<CategoricPhenotypeDTO>();
 
-	public PathologyDTO(Long id, String name) {
+	public PathologyDTO(Long id, String name, Set<NumericPhenotypeDTO> numericPhenotypes, Set<CategoricPhenotypeDTO> categoricPhenotypes) {
 		super();
 		this.setId(id);
 		this.setName(name);
-	}
-
-	public PathologyDTO(String name) {
-		super();
-		this.setName(name);
+		this.setCategoricPhenotypes(categoricPhenotypes);
+		this.setNumericPhenotypes(numericPhenotypes);
 	}
 
 	private PathologyDTO() {
 		super();
-  }
+	}
 
 	/**
 	 * @return the id
@@ -41,12 +43,45 @@ public class PathologyDTO {
 	 */
 	public void setId(Long id) {
 		this.id = id;
-  }
+	}
 
 	/**
 	 * @param name the name to set
 	 */
 	public void setName(String name) {
 		this.name = name;
-  }
+	}
+
+	/**
+	 * @return the categoricPhenotypes
+	 */
+	public Set<CategoricPhenotypeDTO> getCategoricPhenotypes() {
+		return categoricPhenotypes;
+	}
+
+	/**
+	 * @param categoricPhenotypes the categoricPhenotypes to set
+	 */
+	public void setCategoricPhenotypes(Set<CategoricPhenotypeDTO> categoricPhenotypes) {
+		this.categoricPhenotypes = categoricPhenotypes;
+	}
+
+	/**
+	 * @return the numericPhenotypes
+	 */
+	public Set<NumericPhenotypeDTO> getNumericPhenotypes() {
+		return numericPhenotypes;
+	}
+
+	/**
+	 * @param numericPhenotypes the numericPhenotypes to set
+	 */
+	public void setNumericPhenotypes(Set<NumericPhenotypeDTO> numericPhenotypes) {
+		this.numericPhenotypes = numericPhenotypes;
+	}
+
+	public PathologyDTO(String name) {
+		super();
+		this.setName(name);
+	}
 }
--- src/main/java/ar/edu/unlp/pasae/tp_integrador/transformers/PathologyTransformer.java
+++ src/main/java/ar/edu/unlp/pasae/tp_integrador/transformers/PathologyTransformer.java
@@ -1,19 +1,106 @@
 package ar.edu.unlp.pasae.tp_integrador.transformers;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ar.edu.unlp.pasae.tp_integrador.entities.CategoricPhenotype;
+import ar.edu.unlp.pasae.tp_integrador.entities.NumericPhenotype;
 import ar.edu.unlp.pasae.tp_integrador.entities.Pathology;
+import ar.edu.unlp.pasae.tp_integrador.dtos.CategoricPhenotypeDTO;
+import ar.edu.unlp.pasae.tp_integrador.dtos.NumericPhenotypeDTO;
 import ar.edu.unlp.pasae.tp_integrador.dtos.PathologyDTO;
 
 @Service
 public class PathologyTransformer implements Transformer<Pathology, PathologyDTO> {
+	@Autowired
+	private Transformer<NumericPhenotype, NumericPhenotypeDTO> numericPhenotypeTransformer;
+	@Autowired
+	private Transformer<CategoricPhenotype, CategoricPhenotypeDTO> categoricPhenotypeTransformer;
+
 	@Override
 	public Pathology toEntity(PathologyDTO dto) {
-		return new Pathology(dto.getId(), dto.getName());
+		Set<NumericPhenotype> numericPhenotypes = this.numericToEntities(dto.getNumericPhenotypes());
+		Set<CategoricPhenotype> categoricPhenotypes = this.categoricToEntities(dto.getCategoricPhenotypes());
+
+		return new Pathology(dto.getId(), dto.getName(), numericPhenotypes, categoricPhenotypes);
 	}
 
 	@Override
 	public PathologyDTO toDTO(Pathology entity) {
-		return new PathologyDTO(entity.getId(), entity.getName());
+		Set<NumericPhenotypeDTO> numericPhenotypes = this.numericToDtos(entity.getNumericPhenotypes());
+		Set<CategoricPhenotypeDTO> categoricPhenotypes = this.categoricToDtos(entity.getCategoricPhenotypes());
+
+		return new PathologyDTO(entity.getId(), entity.getName(), numericPhenotypes, categoricPhenotypes);
+	}
+
+	private Set<NumericPhenotypeDTO> numericToDtos(Set<NumericPhenotype> phenotypes) {
+		Set<NumericPhenotypeDTO> dtos = new HashSet<NumericPhenotypeDTO>();
+
+		for (NumericPhenotype each : phenotypes) {
+			dtos.add(this.getNumericPhenotypeTransformer().toDTO(each));
+		}
+
+		return dtos;
+	}
+
+	private Set<CategoricPhenotypeDTO> categoricToDtos(Set<CategoricPhenotype> phenotypes) {
+		Set<CategoricPhenotypeDTO> dtos = new HashSet<CategoricPhenotypeDTO>();
+
+		for (CategoricPhenotype each : phenotypes) {
+			dtos.add(this.getCategoricPhenotypeTransformer().toDTO(each));
+		}
+
+		return dtos;
+	}
+
+	private Set<CategoricPhenotype> categoricToEntities(Set<CategoricPhenotypeDTO> phenotypes) {
+		Set<CategoricPhenotype> entities = new HashSet<CategoricPhenotype>();
+
+		for (CategoricPhenotypeDTO each : phenotypes) {
+			entities.add(this.getCategoricPhenotypeTransformer().toEntity(each));
+		}
+
+		return entities;
+	}
+
+	private Set<NumericPhenotype> numericToEntities(Set<NumericPhenotypeDTO> phenotypes) {
+		Set<NumericPhenotype> entities = new HashSet<NumericPhenotype>();
+
+		for (NumericPhenotypeDTO each : phenotypes) {
+			entities.add(this.getNumericPhenotypeTransformer().toEntity(each));
+		}
+
+		return entities;
+	}
+
+	/**
+	 * @return the categoricPhenotypeTransformer
+	 */
+	public Transformer<CategoricPhenotype, CategoricPhenotypeDTO> getCategoricPhenotypeTransformer() {
+		return categoricPhenotypeTransformer;
+	}
+
+	/**
+	 * @param categoricPhenotypeTransformer the categoricPhenotypeTransformer to set
+	 */
+	public void setCategoricPhenotypeTransformer(Transformer<CategoricPhenotype, CategoricPhenotypeDTO> categoricPhenotypeTransformer) {
+		this.categoricPhenotypeTransformer = categoricPhenotypeTransformer;
+	}
+
+	/**
+	 * @return the numericPhenotypeTransformer
+	 */
+	public Transformer<NumericPhenotype, NumericPhenotypeDTO> getNumericPhenotypeTransformer() {
+		return numericPhenotypeTransformer;
+	}
+
+	/**
+	 * @param numericPhenotypeTransformer the numericPhenotypeTransformer to set
+	 */
+	public void setNumericPhenotypeTransformer(Transformer<NumericPhenotype, NumericPhenotypeDTO> numericPhenotypeTransformer) {
+		this.numericPhenotypeTransformer = numericPhenotypeTransformer;
 	}
 }
